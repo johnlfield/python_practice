71f1a6dd11aa33fe63454be1861b8b3eab6f6bbf
johnlfield <johnfield@nrel-105-142.nrel.colostate.edu>
2013-08-12 14:24:12 -0600

    First commit
    Scratch scripts for use with the "How to Think Like a Computer
    Scientist: Learning with Python" (LP) and "Software Carpentry" (SC)
    online courses.  Organize based on chapter/section, with non-active
    code commented out.

---
+++ LPprac.py
@@ -0,0 +1,28 @@
+### INTRO: This file is simply a scratch pad for use going through the 'How to Think 
+# Like a Computer Scientist: Learning with Python' online course. It is organized by the
+# chapter of the online course. 
+
+
+
+### 3. FUNCTIONS
+# def ra():
+#     print 'Ra'
+# 
+# def cheering():
+#     ra()
+#     ra()
+#     ra()
+# 
+# def repeat(chant, times):
+#     print chant*times
+
+# print "Let's start cheering!"
+# cheer = raw_input("What do you want to cheer?  ")               #keeping this as raw input
+# reps = input("How many times do you want to cheer it?  ")
+# print                                                           #prints a blank line
+# repeat(cheer, reps)
+# print
+# print "Okay, that was fun :)"
+
+
+### 4. CONDITIONALS
---
+++ SCprac.py
@@ -0,0 +1,9 @@
+### INTRO: This file is simply a scratch pad for use going through the 'Software 
+# Carpentry' online course sections related to Python. It is organized by the page name 
+# of the online course. 
+
+
+
+### INPUT AND OUTPUT
+
+
